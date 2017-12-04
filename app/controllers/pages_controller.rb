e58263826866b6a230cc89219eab3821982106cd
Bill <billhewitt22@googlemail.com>
2017-12-04 12:36:28 +0000

    heroku

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,80 @@
+class PagesController < ApplicationController
+  
+  def home
+  end
+
+  def engagement
+  end
+
+  def quality
+  end
+
+  def teamwork
+  end
+
+  def reward
+  end
+
+  def training
+  end
+
+  def opportunities
+  end
+
+  def organisation
+  end
+
+  def mywork
+  end
+
+  def management
+  end
+
+  def quality
+  end
+
+  def leadership
+  end
+
+  def communication
+  end
+
+  def survey
+  end
+
+  def stats
+  end
+
+  def action
+  end
+
+  def users
+  end
+ 
+  def teams
+  end 
+
+  def settings
+  end 
+
+  def settings1
+  end 
+
+  def settings2
+  end 
+
+  def profile
+  end 
+
+  def profile
+  end 
+
+  def usersnew
+  end
+
+  def teamsnew
+  end 
+
+  def compare
+  end 
+end
